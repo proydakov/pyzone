67a1a03e897284426e64d01ecfcef3f9f6232a75
Evgeny Proydakov <e.proydakov@gmail.com>
2026-04-02 14:51:35 +0300

    В реализацию my_rust_module добавлены функции по обработке bytes и bytearray
    Signed-off-by: Evgeny Proydakov <e.proydakov@gmail.com>

--- my_rust_module/src/lib.rs
+++ my_rust_module/src/lib.rs
@@ -1,4 +1,6 @@
 use pyo3::prelude::*;
+use pyo3::types::PyBytes;
+use pyo3::types::PyByteArray;
 
 /// A Rust function exposed to Python that adds two numbers and returns the result as a string.
 #[pyfunction]
@@ -18,6 +20,18 @@ fn say(name: &str) {
     println!("{}", name);
 }
 
+/// Method with a bytes input.
+#[pyfunction]
+fn handle_bytes(bytes: &Bound<'_, PyBytes>) {
+    println!("handle bytes [{}]", bytes.len().unwrap());
+}
+
+/// Method with a bytearray input.
+#[pyfunction]
+fn handle_bytearray(bytes: &Bound<'_, PyByteArray>) {
+    println!("handle bytearray [{}]", bytes.len());
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn my_rust_module(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
@@ -25,5 +39,7 @@ fn my_rust_module(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(greet, m)?)?;
     m.add_function(wrap_pyfunction!(say, m)?)?;
+    m.add_function(wrap_pyfunction!(handle_bytes, m)?)?;
+    m.add_function(wrap_pyfunction!(handle_bytearray, m)?)?;
     Ok(())
 }
