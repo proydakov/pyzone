9a20da35c72a9c704f69eb185658c9bdc74ca64a
Evgeny Proydakov <e.proydakov@gmail.com>
2026-04-03 17:31:40 +0300

    Added classes Speeker, T800 & T1000 in my_rust_module + up version
    Signed-off-by: Evgeny Proydakov <e.proydakov@gmail.com>

--- my_rust_module/src/lib.rs
+++ my_rust_module/src/lib.rs
@@ -32,14 +32,105 @@ fn handle_bytearray(bytes: &Bound<'_, PyByteArray>) {
     println!("handle bytearray [{}]", bytes.len());
 }
 
+/// Speeker
+#[derive(Clone)]
+#[pyclass]
+struct Speeker {
+    #[pyo3(get)]
+    name: String,
+
+    #[pyo3(get)]
+    version: String,
+}
+
+/// Speeker
+#[pymethods]
+impl Speeker {
+    #[new]
+    fn new(name: String, version: String) -> Self {
+        Speeker { name, version }
+    }
+
+    fn talk(&self) {
+        println!("Talking [{}] with Speech version {}.", self.name, self.version);
+    }
+}
+
+/// T800
+#[pyclass]
+struct T800 {
+    speeker: Speeker,
+    
+    #[pyo3(get)] // Exposes this field as a Python property
+    name: String,
+
+    #[pyo3(get, set)] // Exposes this field as a Python property
+    year: i32,
+}
+
+/// T800
+#[pymethods]
+impl T800 {
+    #[new]
+    fn new(name: String, year: i32, speeker: Speeker) -> Self {
+        T800 { name, year, speeker }
+    }
+
+    fn walk(&self) {
+        println!("T-800 is walking...");
+    }
+
+    fn talk(&self) {
+        print!("{} ", self.name);
+        self.speeker.talk();
+    }
+}
+
+/// T1000
+#[pyclass]
+struct T1000 {
+    speeker: Speeker,
+
+    #[pyo3(get)] // Exposes this field as a Python property
+    name: String,
+
+    #[pyo3(get, set)] // Exposes this field as a Python property
+    height: f64,
+}
+
+/// T1000
+#[pymethods]
+impl T1000 {
+    #[new]
+    fn new(name: String, height: f64, speeker: Speeker) -> Self {
+        T1000 { name, height, speeker }
+    }
+
+    fn walk(&self) {
+        println!("T-1000 is walking...");
+    }
+
+    fn talk(&self) {
+        print!("{} ", self.name);
+        self.speeker.talk();
+    }
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn my_rust_module(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
+
     // Add the functions to the module
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(greet, m)?)?;
     m.add_function(wrap_pyfunction!(say, m)?)?;
     m.add_function(wrap_pyfunction!(handle_bytes, m)?)?;
     m.add_function(wrap_pyfunction!(handle_bytearray, m)?)?;
+
+    // Add the classes to the module
+    m.add_class::<Speeker>()?;
+    m.add_class::<T800>()?;
+    m.add_class::<T1000>()?;
+
     Ok(())
 }
