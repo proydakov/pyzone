4d831f732719ea4b1dda1d312d11e18789a39089
Evgeny Proydakov <e.proydakov@gmail.com>
2026-04-03 18:00:42 +0300

    Updated __init__.py in my_cxx_module
    Signed-off-by: Evgeny Proydakov <e.proydakov@gmail.com>

--- my_cxx_module/src/my_cxx_module/__init__.py
+++ my_cxx_module/src/my_cxx_module/__init__.py
@@ -3,12 +3,8 @@
 __version__ = "0.7.5"
 
 import os
-import typing
 
-if not typing.TYPE_CHECKING and os.getenv("PYBIND11_PROJECT_PYTHON_DEBUG"):
+if os.getenv("PYBIND11_PROJECT_PYTHON_DEBUG"):
     from .module_d import *  # noqa: F403
-    from .module_d import __version__  # noqa: F401, RUF100
 else:
     from .module import *  # noqa: F403
-    from .module import __version__  # noqa: F401, RUF100
-
